2f71ef10f8d6d5d1e250643e73a024fb5624eb38
rassulmurat <rasusl1995@gmail.com>
2016-10-20 16:25:49 +0300

    All warnings were fixed

--- sysfs_timer.c
+++ sysfs_timer.c
@@ -2,17 +2,20 @@
 #include <linux/module.h>
 #include "sysfs_timer.h"
 
-static ssize_t showFile(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t showFile(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	printk("TimerNum: %d\n", timerNum);
 	sprintf(buf,"%d", timerNum);
+
 	return 0;
 }
 
-static ssize_t storeFile(struct device *dev, struct device_attribute *attr,
-	 const char *buf, size_t count)
+static ssize_t storeFile(struct kobject *kobj, struct kobj_attribute *attr, 
+			const char *buf, size_t count)
 {
+	printk("Im trying to write\n");
 	convert(buf, count, &timerNum);
+	// printk("Count: %d\n", count);
 	return count;
 }
 
@@ -21,8 +24,9 @@ int convert(const char *buf, int count, int *res)
     int i = 0;
     int tmp;
     int sum = 0;
-    while (i < count) {
-        int power = count - 1 - i;
+    printk("Im converting\n");
+    while (i < count - 1) {
+        int power = count - 2 - i;
         int rlpwr = 1;
         while (power > 0) {
             rlpwr = rlpwr * 10;
@@ -33,6 +37,7 @@ int convert(const char *buf, int count, int *res)
         ++i;
     }
     *res = sum;
+    printk("Sum: %d\n", sum);
     return 0;
 }
 
@@ -41,18 +46,19 @@ struct kobj_attribute kobjAttr = {
 		.name = "timer_prnt",
 		.mode = 0777
 	},
+	.store = storeFile,
 	.show = showFile,
-	.store = storeFile
 };
 
 static int __init timer_init(void)
 {
+	int res;
 	kobj = kobject_create_and_add("timer_prnt", NULL);
 	if (!kobj) {
 		printk("ERROR: Could not create kobject");
 		return -ENOMEM;
 	}
-	int res = sysfs_create_file(kobj, &kobjAttr);
+	res = sysfs_create_file(kobj, &kobjAttr.attr);
 	if (res) {
 		printk("ERROR: Could not create file");
 		return -ENOMEM;
